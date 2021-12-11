bdcdffbd1fb63d203948841e23e8bfa875107a22
k-e-r <damajonboua@gmail.com>
2021-12-11 12:31:14 -0800

    changing behavior of input value and display movie details

--- movie/index.js
+++ movie/index.js
@@ -47,6 +47,11 @@ const onInput = async (event) => {
       <img src="${imgSrc}" />
       ${movie.Title}
     `;
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+      onMovieSelect(movie);
+    });
 
     resultsWrapper.appendChild(option);
   }
@@ -59,3 +64,33 @@ document.addEventListener('click', (event) => {
     console.log(event.target);
   }
 });
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'ae1a0754',
+      i: movie.imdbID,
+    },
+  });
+
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+  `;
+};
