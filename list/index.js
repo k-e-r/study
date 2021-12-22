4fcc30de6ba68b618d3a7f6cdfe02b08e87dd900
k-e-r <damajonboua@gmail.com>
2021-12-22 00:42:21 -0800

    add solution of ls command by node.js

---
+++ list/index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const util = require('util');
+
+// Method #2
+// const lstat = util.promisify(fs.lstat);
+
+// Method #3
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
+  if (err) {
+    console.log(err);
+    // throw new Error(err);
+  }
+
+  for (let filename of filenames) {
+    try {
+      const stats = await lstat(filename);
+
+      console.log(filename, stats.isFile());
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});
+
+// Method #1
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+
+//       resolve(stats);
+//     });
+//   });
+// };
