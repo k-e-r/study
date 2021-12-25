27653ab81c127c8ed2003e7d19846d1d96978bac
k-e-r <damajonboua@gmail.com>
2021-12-25 15:59:48 -0800

    add delete function with id

--- ecomm/repositories/users.js
+++ ecomm/repositories/users.js
@@ -48,16 +48,23 @@ class UsersRepository {
     const records = await this.getAll();
     return records.find((record) => record.id === id);
   }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter((record) => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  const user = await repo.getOne('4af1f1a0');
+  await repo.delete('4af1f1a0');
+  // const user = await repo.getOne('4af1f1a0');
   // await repo.create({ email: 'test@test.com', password: 'pass' });
   // const users = await repo.getAll();
 
-  console.log(user);
+  // console.log(user);
 };
 
 test();
